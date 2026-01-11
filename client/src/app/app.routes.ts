6430051c82d86618ff6812838e3f0bf051a5503b
Akshay <akshaykm1100@gmail.com>
2026-01-11 13:58:11 +0530

    error handling

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailed } from '../features/members/member-detailed/member-detai
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guard/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
 
 export const routes: Routes = [
     {path: '', component: Home},
@@ -17,9 +20,11 @@ export const routes: Routes = [
             {path: 'members/:id', component: MemberDetailed},
             {path: 'lists', component: Lists},
             {path: 'messages', component: Messages},
-            {path: '**', component: Home},
+   
         ]
     },
-
+{ path: 'test-errors', component: TestErrors    },
+{ path: 'server-error', component: ServerError },
+{ path: '**', component: NotFound }
   
 ];
